62fe6063062995d43158002cf7644f34223bcc3f
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2020-12-21 16:19:21 +0300

    feat(general): init commit + view post functionality

---
+++ src/app/shared/modules/backendErrorMessages/components/backendErrorMessages.component.ts
@@ -0,0 +1,21 @@
+import { BackendErrorsInterface } from './../../../types/backendErrors.interface';
+import { Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'mc-backend-error-messages',
+  templateUrl: './backendErrorMessages.component.html',
+  styleUrls: ['./backendErrorMessages.component.scss']
+})
+export class BackendErrorMessagesComponent implements OnInit {
+  @Input('backendErrors') backendErrorsProps: BackendErrorsInterface;
+
+  errorMessages: string[];
+
+  ngOnInit(): void {
+    this.errorMessages = Object.keys(this.backendErrorsProps).map((name: string) => {
+      const messages = this.backendErrorsProps[name].join(', ')
+
+      return `${name} ${messages}`
+    });
+  }
+}
